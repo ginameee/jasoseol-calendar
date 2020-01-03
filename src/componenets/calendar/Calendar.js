2b20cfc6259af641b9db81c2628c86782d5695cf
Chun <ginameeee@gmail.com>
2020-01-03 12:16:53 +0900

    Highlight today

--- src/componenets/calendar/Calendar.js
+++ src/componenets/calendar/Calendar.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import './Calendar.css';
 
+import Content from './CalendarContent';
+
+const today = new Date();
+
 class Calendar extends React.Component {
+    state = {
+        year: today.getFullYear(),
+        month: today.getMonth(),
+        date: today.getDate()
+    }
+
     render() {
         return (
             <div id="calendar">
-            <div id="header">
-                <button>이전 월</button>
-                <h2>2019년 4월</h2>
-                <button>다음 월</button>
-             </div>
-            <div className="content">
-                Body
-            </div>
+                <hr></hr>
+                <div id="header">
+                    <button>이전 월</button>
+                    <h2>{this.state.year}년 {this.state.month + 1}월</h2>
+                    <button>다음 월</button>
+                    <button id="btn_add">추가하기</button>
+                </div>
+                <Content year={this.state.year} month={this.state.month} date={this.state.date}/>
             </div>
         );
     }
--- src/componenets/calendar/CalendarContent.js
+++ src/componenets/calendar/CalendarContent.js
@@ -3,49 +3,91 @@ import './CalendarContent.css';
 
 class CalendarContent extends React.Component {
     state = {
-        calWidth: 7,
+        calColCnt: 7,
+        calRowCnt: 5,
+        calLength: 35
+    }
+
+    /**
+     * 날짜에 맞는 스케쥴정보를 가져온다.
+     * @param {Date} dateObj 
+     */
+    getSchedule(dateObj) {
+        return ( <span>gg</span> );
     }
 
     renderHeader() {
         const days = [ '일', '월', '화', '수', '목', '금', '토' ];
         let headerCells = [];
 
-        for (let i = 0; i < this.state.calWidth; i++) {
-            headerCells.push(
-                <th>{days[i % this.state.calWidth % days.length]}</th>
-            );
-        }
+        days.forEach(
+            (day) => {
+                headerCells.push(
+                    <th>{day}</th>
+                );
+            }
+        )
 
-        return headerCells;
+        return (
+            <tr id="c_header">
+                {headerCells}
+            </tr>
+        );
     }
 
     renderContent() {
-        let dateCells = [];
+        let calRows = [];
+        let dateObj = new Date(this.props.year, this.props.month, 1);
+        let tempDateObj; 
+        let strDay = new Date(this.props.year, this.props.month, 1).getDay();
 
-        for (let j = 0; j < this.state.calWidth; j++) {
-            dateCells.push(
-                <td>
-                    <div>
-                        {j}
-                    </div>
-                    <div></div>
-                </td>
+        for (let i = 0; i < this.state.calRowCnt; i++) {
+            calRows.push(
+                <tr id="c_content">
+                    {
+                        Array(7).fill(0).map(
+                            (item, idx) => {
+                                let cellIdx = this.state.calColCnt * i + idx;
+                                let date;
+
+                                if (!tempDateObj || cellIdx < strDay) {
+                                    tempDateObj = new Date(dateObj.getTime());
+                                    tempDateObj.setDate(dateObj.getDate() - (strDay - cellIdx));
+                                }
+                                else {
+                                    tempDateObj.setDate(tempDateObj.getDate() + 1);
+                                }
+                                
+                                date = tempDateObj.getDate();
+
+                                return (
+                                    <td className={
+                                        (tempDateObj.getMonth() === new Date().getMonth() && tempDateObj.getDate() === new Date().getDate()) ? "today" : ""
+                                        }>
+                                        <div className="date">
+                                            {date}
+                                        </div>              
+                                        <div className="schedule">
+                                            {this.getSchedule(tempDateObj)}
+                                        </div>
+                                    </td>
+                                );
+                            }
+                        )
+                    }
+                </tr>
             );
         }
 
-        return dateCells;
+        return calRows;
     }
 
     render() {
         return (
             <div id="calendar_content">
                 <table>
-                    <tr id="c_header">
-                        {this.renderHeader()}
-                    </tr>
-                    <tr id="c_content">
-                        {this.renderContent()}
-                    </tr>
+                    {this.renderHeader()}
+                    {this.renderContent()}
                 </table>
             </div>
         );
